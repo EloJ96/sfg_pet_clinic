9607f1267cdab2be01b350a828b5628f7f36b0c3
EloJ96 <kelvin.piras@we-plus.eu>
2022-11-28 11:37:17 +0100

    Implement Interfaces for Services. Closes #5

---
+++ pet_clinic_data/src/main/java/weplus/sfg_pet_clinic/services/VetService.java
@@ -0,0 +1,15 @@
+package weplus.sfg_pet_clinic.services;
+
+import weplus.sfg_pet_clinic.model.Owner;
+import weplus.sfg_pet_clinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
