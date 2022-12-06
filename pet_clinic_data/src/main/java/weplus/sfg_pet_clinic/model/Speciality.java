ca0098aa22474bfa80b20f400964a5e03870b557
EloJ96 <kelvin.piras@we-plus.eu>
2022-12-06 09:23:59 +0100

    Create Vet Speciality, Entity, Associate to Vet. Closes #27

---
+++ pet_clinic_data/src/main/java/weplus/sfg_pet_clinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package weplus.sfg_pet_clinic.model;
+
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
