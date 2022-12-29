ad5905d61cf868c420554e6a012fca53edfc2f54
Kelvin Piras <kelvin.piras@we-plus.eu>
2022-12-29 10:15:28 +0100

    Added Spring data JPA PetType Service. Closes #42

---
+++ pet_clinic_data/src/main/java/weplus/sfg_pet_clinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,48 @@
+package weplus.sfg_pet_clinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import weplus.sfg_pet_clinic.model.PetType;
+import weplus.sfg_pet_clinic.repositories.PetTypeRepository;
+import weplus.sfg_pet_clinic.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
