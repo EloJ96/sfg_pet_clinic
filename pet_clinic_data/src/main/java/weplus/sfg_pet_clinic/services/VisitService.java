b6d7815817fad94c86206bb8ec60a1229e73a45f
Kelvin Piras <kelvin.piras@we-plus.eu>
2022-12-29 10:47:53 +0100

    Create Visit Map Service. Closes #46

---
+++ pet_clinic_data/src/main/java/weplus/sfg_pet_clinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package weplus.sfg_pet_clinic.services;
+
+import weplus.sfg_pet_clinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet_clinic_data/src/main/java/weplus/sfg_pet_clinic/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package weplus.sfg_pet_clinic.services.map;
+
+
+import weplus.sfg_pet_clinic.model.Visit;
+import weplus.sfg_pet_clinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long>
+        implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if (object.getPet() == null ||
+            object.getPet().getOwner() == null ||
+            object.getPet().getId() == null ||
+            object.getPet(). getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
